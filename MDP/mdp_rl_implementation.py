5fbd438d34304a323bd39e5d694210ea572cf2ec
zofit <zofit648@gmail.com>
2024-08-21 13:16:27 +0300

    bug fix get_policy and value_iteration

--- MDP/mdp_rl_implementation.py
+++ MDP/mdp_rl_implementation.py
@@ -6,31 +6,44 @@ import numpy as np
 
 def find_max_action(mdp: MDP, i, j, U_local):
     max_sum = float('-inf')
-    for action, i in enumerate(mdp.actions):
+    if (i, j) in mdp.terminal_states:
+        return 0
+    for action in mdp.actions.keys():
         sum_action = 0
-        for action2, j in enumerate(mdp.actions):
+        count = 0
+        for action2, value2 in mdp.actions.items():
             next_state = mdp.step((i, j), action2)
-            sum_action += mdp.transition_function[i][j] * U_local[next_state[0]][next_state[i]]
-        max_sum = max(sum_action, sum_action)
+            y = mdp.transition_function[action][count]
+            x = U_local[next_state[0]][next_state[1]]
+            sum_action += mdp.transition_function[action][count] * U_local[next_state[0]][next_state[1]]
+            count += 1
+        max_sum = max(max_sum, sum_action)
     return max_sum
 
 
-def value_iteration(mdp: MDP, U_init: np.ndarray, epsilon: float = 10 ** (-3)) -> np.ndarray:  # TODO wall
+def value_iteration(mdp: MDP, U_init: np.ndarray, epsilon: float = 10 ** (-3)) -> np.ndarray:
     # Given the mdp, the initial utility of each state - U_init,
     #   and the upper limit - epsilon.
     # run the value iteration algorithm and
     # return: the utility for each of the MDP's state obtained at the end of the algorithms' run.
     #
     U_local = U_init
-    U_final = None
+    U_final = []
+    for i in range(mdp.num_row):
+        U_final.append([0] * mdp.num_col)
     while True:
         delta = 0
-        U_final = U_local
         for i in range(3):
             for j in range(4):
-                U_local[i][j] = mdp.board[i][j] + mdp.gamma * find_max_action(mdp, i, j, U_final)
-                if abs(U_local[i][j] - U_final[i][j]) > delta:
-                    delta = abs(U_local[i][j] - U_final[i][j])
+                U_final[i][j] = U_local[i][j]
+        for i in range(3):
+            for j in range(4):
+                if mdp.board[i][j] != 'WALL':
+                    U_local[i][j] = float(mdp.board[i][j]) + mdp.gamma * find_max_action(mdp, i, j, U_final)
+                    if abs(U_local[i][j] - U_final[i][j]) > delta:
+                        delta = abs(U_local[i][j] - U_final[i][j])
+                else:
+                    U_local[i][j] = None
         if delta < epsilon * (1 - mdp.gamma) / mdp.gamma:
             break
     return U_final
@@ -40,18 +53,22 @@ def get_policy(mdp: MDP, U: np.ndarray) -> np.ndarray:
     # Given the mdp and the utility of each state - U (which satisfies the Belman equation)
     # return: the policy
     #
-    policy = None
+    policy = []
+    for i in range(mdp.num_row):
+        policy.append([0] * mdp.num_col)
     for i in range(3):
         for j in range(4):
-            if mdp.board[i][j] == "WALL" or (i, j) in mdp.terminal_states:
+            max_action = None
+            if mdp.board[i][j] == 'WALL' or (i, j) in mdp.terminal_states:
                 policy[i][j] = None
             else:
-                max_action = None
                 max_utility = float('-inf')
-                for action in mdp.actions:
+                for action in mdp.actions.keys():
                     next_state = mdp.step((i, j), action)
-                    if max_utility > U[next_state[0], next_state[1]]:
-                        max_utility = U[next_state[0], next_state[1]]
+                    if mdp.board[next_state[0]][next_state[1]] == 'WALL':
+                        continue
+                    if max_utility < U[next_state[0]][next_state[1]]:
+                        max_utility = U[next_state[0]][next_state[1]]
                         max_action = action
                 policy[i][j] = max_action
     return policy
